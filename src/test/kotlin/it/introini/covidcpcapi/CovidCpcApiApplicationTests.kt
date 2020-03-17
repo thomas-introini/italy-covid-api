7d93be9c04625110caf4eb2780487471f86a1e29
thomas <introt@gmail.com>
2020-03-17 18:10:27 +0100

    remove unused import

--- src/test/kotlin/it/introini/covidcpcapi/CovidCpcApiApplicationTests.kt
+++ src/test/kotlin/it/introini/covidcpcapi/CovidCpcApiApplicationTests.kt
@@ -1,9 +1,5 @@
 package it.introini.covidcpcapi
 
-import jdk.nashorn.internal.ir.annotations.Ignore
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Profile
 import org.springframework.test.context.ActiveProfiles
 
 @ActiveProfiles("test")
